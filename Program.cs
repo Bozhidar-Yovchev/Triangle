f4f301c520c979bd9e3238b5f2ab110cb9f1ef87
Bozhidar-Yovchev <bozhidar601@gmail.com>
2024-09-09 14:57:28 +0300

    better visualization

--- Program.cs
+++ Program.cs
@@ -10,12 +10,13 @@ namespace Triangle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter side a: ");
+            Console.Write("Enter side a: ");
             int a = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter side b:  "); //side b 
+            Console.Write("Enter side b: "); 
             int b = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter side c:  ");
+            Console.Write("Enter side c: ");
             int c = int.Parse(Console.ReadLine());
+            Console.WriteLine();
             Triangle triangle = new Triangle(a,b,c);
             Console.WriteLine($"Perimeter of the triangle is {triangle.P()}");
             Console.WriteLine($"Area of the triagle is {triangle.S():f2}");
