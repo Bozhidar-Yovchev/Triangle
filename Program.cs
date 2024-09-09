6c9fa0cd10822b0765d09b1468cfa3481bfc3b37
Bozhidar-Yovchev <bozhidar601@gmail.com>
2024-09-09 14:03:39 +0300

    Perimeter and Area of triangle

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter side a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter side b:  ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter side c:  ");
+            int c = int.Parse(Console.ReadLine());
+            Triangle triangle = new Triangle(a,b,c);
+            Console.WriteLine($"Perimeter of the triangle is {triangle.P()}");
+            Console.WriteLine($"Area of the triagle is {triangle.S():f2}");
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+
+{
+    class Triangle
+
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public double A
+        {
+            get { return a;}
+            set { a = value; }
+        }
+        public double B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+        public double C
+        {
+            get { return c; }
+            set { c = value; }
+        }
+        public Triangle()
+        {
+            A = 0;
+            B = 0;
+            C = 0;
+        }
+
+        public Triangle(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+        public double P()
+        {
+            return a+b+c;
+        }
+        public double S()
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt((p-a)*(p-b)*(p-c));
+        }
+
+    }
+}
